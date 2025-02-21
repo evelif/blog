5ec577d1b09b50a208a11b061fe64150e7281639
ondojung <ondojung@gmail.com>
2025-02-22 03:46:17 +0900

    FIX: SUSPENSE ERROR

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api",
+    process.env.NEXT_PUBLIC_API_BASE_URL || "http://152.70.250.19:3000/api",
   headers: {
     "Content-Type": "application/json",
   },
--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,5 +1,6 @@
 import ListBlock from "@/components/Archive/ListBlock"
 import ProfileBlock from "@/components/Common/ProfileBlock";
+import { Suspense } from "react";
 
 export default function TagDetail(){
     
@@ -9,7 +10,9 @@ export default function TagDetail(){
                           github="hayonn"
                           email="seohayeon.kr@gmail.com" 
                           avatar="https://avatars.githubusercontent.com/u/190105697?s=400&u=43e6fbe9b66f60c570e0caccba86541486254573&v=4"/>
-            <ListBlock/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <ListBlock/>
+            </Suspense>
         </>
         )
 }
