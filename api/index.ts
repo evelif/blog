fb8934495d2c1087269bbdd9315f57c09d5e6251
ondojung <ondojung@gmail.com>
2025-02-22 04:00:45 +0900

    -

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_BASE_URL || "http://152.70.250.19:3000/api",
+  baseURL:"/api",
   headers: {
     "Content-Type": "application/json",
   },
